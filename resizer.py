2461137c82384c84181de104235de5beccd281c7
Alex Phan <ahphan@umich.edu>
2019-02-14 17:36:07 -0500

    reworked resizer to a function call
    Signed-off-by: Alex Phan <ahphan@umich.edu>

--- resizer.py
+++ resizer.py
@@ -1,19 +1,28 @@
 # this program resizes the raw images, using bicubic interpolation
-
 import os
 
 from PIL import Image
 
-raw = 'data/raw'
-resize = 'data/resize'
+# this function resizes with bicubic Interpolation
+def resize_image_set(source='data/raw', destination='data/resize', extension='.jpg'):
+		
+	raw_files = [i for i in os.listdir(source) \
+					if os.path.isfile(os.path.join(source, i))]
+	num_resize = len(raw_files)
 
-raw_files = [i for i in os.listdir(raw) if os.path.isfile(os.path.join(raw, i))]
-index = 0
-extension = ".jpg"
+	# check if resize has already been done
+	resized_files = [i for i in os.listdir(destination) \
+						if os.path.isfile(os.path.join(destination, i))]
 
-for file_ in raw_files:
-	img = Image.open(os.path.join(raw, file_))
-	img_resize = img.resize((64, 64), resample=Image.BICUBIC)
-	filepath = "resize" + str(index) + extension
-	img_resize.save(os.path.join(resize, filepath))
-	index += 1
+	if len(resized_files) == num_resize:
+		print("resize on raw images has already been done at this destination")
+	else:
+		index = 0
+
+		for file_ in raw_files:
+			img = Image.open(os.path.join(raw, file_))
+			img_resize = img.resize((64, 64), resample=Image.BICUBIC)
+			filepath = "resize" + str(index) + extension
+			img_resize.save(os.path.join(resize, filepath))
+			index += 1
+
